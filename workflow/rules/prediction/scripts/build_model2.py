3a6344c380e338830f8fd1037a48416f20837d94
fguntoro <fernando_guntoro@hotmail.com>
2021-07-06 11:55:46 +0100

    Add first version for merge

--- workflow/rules/prediction/scripts/build_model2.py
+++ workflow/rules/prediction/scripts/build_model2.py
@@ -3,25 +3,30 @@ import argparse
 import sys
 import os
 import utils
-from support import saveObject
 
 def main(sysargs=sys.argv[1:]):
+    print("_______________________________")
+    print("Building model")
+    print("_______________________________")
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
-            '--config', dest='config', metavar='FILE',
-            help='Path to the config file', required=True,
-        )
+        '--config', dest='config', metavar='FILE',
+        help='Path to the config file', required=True,
+    )
     parser.add_argument(
         '--model-name', dest='models', nargs='+',
         help='Name of model in config file', required=True
-    )
+        )
+
     args = parser.parse_args()
     models = args.models
     print("________________")
     print(models)
 
+    config_file = utils.config_reader(args.config)
+    
     for model in models:
-        config_file = utils.config_reader(args.config)
         current_module = utils.my_import(config_file['Models'][model]['module'])
         dClassifier = getattr(current_module, config_file['Models'][model]['model'])
         dClassifier = dClassifier(**config_file['Models'][model]['params'])
