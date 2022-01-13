e3597d75434155b03fe35c188207f4cfbabe5079
nik1sto <nik1609@msn.com>
2022-01-13 16:31:46 +0000

    bugfixes

--- workflow/scripts/prediction/aggregate.py
+++ workflow/scripts/prediction/aggregate.py
@@ -6,15 +6,19 @@ import re
 input_files = snakemake.input
 output_file = str(snakemake.output)
 
-def main():
-    summary = pd.DataFrame()
+config = snakemake.params["conf"]
+print(config)
 
-    for file in input_files:
-        df = pd.read_csv(file)
-        summary = pd.concat([summary, df], axis=0)
+
+# def main():
+#     summary = pd.DataFrame()
+
+#     for file in input_files:
+#         df = pd.read_csv(file)
+#         summary = pd.concat([summary, df], axis=0)
     
-    print(summary)
-    print("Saving summary to ", output_file)
-    summary.to_csv(output_file, index=False)
+#     print(summary)
+#     print("Saving summary to ", output_file)
+#     summary.to_csv(output_file, index=False)
 
-main()
+# main()
