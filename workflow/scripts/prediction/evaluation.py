895b61960321bd82e1adc1aad6867bc86528e30e
nik1sto <nik1609@msn.com>
2022-01-19 19:18:28 +0000

    run different representations without hardcoding

--- workflow/scripts/prediction/evaluation.py
+++ workflow/scripts/prediction/evaluation.py
@@ -7,23 +7,30 @@ import os
 
 data_file = snakemake.input["data"]
 labels_file = snakemake.input["label"]
-model= snakemake.input["model"]
+model = snakemake.input["model"]
 method = snakemake.wildcards.method
 method_config_file = snakemake.input["conf"]
+drug = snakemake.wildcards.drug
+representation = snakemake.wildcards.representation
 
-drug = snakemake.params["drug"]
 
 mode = snakemake.config["MODE"]
 optimization = snakemake.config["OPTIMIZATION"]
 
 output_file = str(snakemake.output)
 
+
 def main():
     from sklearn.model_selection import train_test_split
     from skopt import BayesSearchCV
-    from support import (preprocess, model_fitting, evaluate_classifier, evaluate_regression)
+    from support import (
+        preprocess,
+        model_fitting,
+        evaluate_classifier,
+        evaluate_regression,
+    )
 
-    data, labels = preprocess(data = data_file, label = labels_file)
+    data, labels = preprocess(data=data_file, label=labels_file)
     config_file = utils.config_reader(method_config_file)
 
     print("Data file = ", data_file)
@@ -34,56 +41,62 @@ def main():
     print("Optimization = ", optimization)
     print("_______________________________")
 
-    
-    results=pd.DataFrame()
+    results = pd.DataFrame()
 
-    print('Analysing {0}'.format(drug))
+    print("Analysing {0}".format(drug))
 
     y = np.ravel(labels[[drug]])
     na_index = np.isnan(y)
     y = y[~na_index]
 
     X = data
-    X = X.loc[~na_index,:]
+    X = X.loc[~na_index, :]
 
-    print('Data shape: {}'.format(X.shape))
-    print('Label shape: {}'.format(y.shape))
+    print("Data shape: {}".format(X.shape))
+    print("Label shape: {}".format(y.shape))
     print("_______________________________")
 
-    print('Train-Test split')
-    X_train, X_test, y_train, y_test = train_test_split(X, y, **config_file['TrainTestSplit'])
-    print('Train data shape: {}'.format(X_train.shape))
-    print('Train label shape: {}'.format(y_train.shape))
-    print('Test data shape: {}'.format(X_test.shape))
-    print('Test label shape: {}'.format(y_test.shape))
+    print("Train-Test split")
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, **config_file["TrainTestSplit"]
+    )
+    print("Train data shape: {}".format(X_train.shape))
+    print("Train label shape: {}".format(y_train.shape))
+    print("Test data shape: {}".format(X_test.shape))
+    print("Test label shape: {}".format(y_test.shape))
     print("_______________________________")
 
-    print('Fitting training data')
+    print("Fitting training data")
     start_time = time.time()
-    clf, best_parameters = model_fitting(drug, X_train, y_train, method, model, optimization, config_file, output_file)
+    clf, best_parameters = model_fitting(
+        drug, X_train, y_train, method, model, optimization, config_file, output_file
+    )
     end_time = time.time()
     print("_______________________________")
 
-    print('Testing')
+    print("Testing")
     y_pred = clf.predict(X_test)
     print("_______________________________")
 
-    print('Evaluating')
+    print("Evaluating")
 
     if mode == "Classification":
-      result = evaluate_classifier(y_test, y_pred)
-    
+        result = evaluate_classifier(y_test, y_pred)
+
     if mode == "MIC":
-      result = evaluate_regression(y_test, y_pred)
+        result = evaluate_regression(y_test, y_pred)
 
     result.insert(0, "Method", method)
     result.insert(1, "Parameters", best_parameters)
-    result.insert(2, "Drug", drug)
-    result.insert(3, "Time", end_time-start_time)
+    result.insert(2, "Representation", representation)
+    result.insert(3, "Drug", drug)
+    result.insert(4, "Time", end_time - start_time)
+
     results = results.append(result)
     print("_______________________________")
 
-    print('Saving results to {0}'.format(output_file))
-    results.to_csv(output_file, index =False)
-            
+    print("Saving results to {0}".format(output_file))
+    results.to_csv(output_file, index=False)
+
+
 main()
